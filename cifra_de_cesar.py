aa7e2a6f1efc4bec49158e8b229b84cf9307822f
Clovis David <orcsdzn@gmail.com>
2023-10-05 23:22:17 -0300

    pequena alteração nos comentários

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -1,8 +1,10 @@
+#Uma pequena adaptação da Cifra de César utilizando o endereço de caracteres UNICODE
 operacoes = {}
 
 def criptografar(texto, shift):
     texto_criptografado = ''
 
+    #criptografa todos os caracteres UNICODE com edereço de 32 a 126 e 160 a 255
     for caractere in texto:
         caractere_com_shift = ord(caractere) + shift #faz um shift no caractere atual
 
@@ -16,7 +18,6 @@ def criptografar(texto, shift):
                 #concatena o caractere a variável texto_criptografado, caso ele esteja dentro dos limites
                 texto_criptografado += chr(caractere_com_shift)
 
-        #criptografa todos os caracteres com endereço de 160 a 255 na tabela UNICODE
         if ord(caractere) >= 160 and ord(caractere) <= 255:
             if caractere_com_shift > 255:
                 texto_criptografado += chr(caractere_com_shift - 96)
@@ -30,25 +31,25 @@ def criptografar(texto, shift):
 def descriptografar(texto, shift, bruteforcing):
     texto_descriptografado = ''
 
+    #descriptografa todos os caracteres UNICODE com edereço de 32 a 126 e 160 a 255
     for caractere in texto:
         caractere_sem_shift = ord(caractere) - shift #faz um shift no caractere atual
-        #descriptografa todos os caracteres com edereço de 32 a 126 na tabela UNICODE
         if ord(caractere) >= 32 and ord(caractere) <= 126:
             if caractere_sem_shift < 32:
                 texto_descriptografado += chr(caractere_sem_shift + 95)
             else:
                 texto_descriptografado += chr(caractere_sem_shift)
 
-        #descriptografa todos os caracteres com edereço de 160 a 255 na tabela UNICODE
         if ord(caractere) >= 160 and ord(caractere) <= 255:
             if caractere_sem_shift < 160:
                 texto_descriptografado += chr(caractere_sem_shift + 95)
             else:
                 texto_descriptografado += chr(caractere_sem_shift)
-        
+
+    #checa se a função não foi chamada pela função bruteforce, assim evitando criar múltiplos registros iguais,
+    #devido ao for contido em bruteforce que invoca a função descriptografar múltiplas vezes  
     if not bruteforcing:
         registrar_operacao(texto, texto_descriptografado, 'descriptografia', shift)
-
 
     return texto_descriptografado
 
@@ -104,6 +105,7 @@ def registrar_operacao(texto_origem, texto_final, tipo, shift):
         'shift_utilizado': shift
     }
 
+#ao fim mostra histórico de operações de criptografia, descriptografia e bruteforce realizados durante a execução
 def mostrar_historico():
     print('\n\n-----Histórico de operações:')
     for operacao in operacoes:
